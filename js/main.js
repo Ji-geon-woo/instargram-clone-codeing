21106ee01c6a26d23a9d0846015b4d2602f0b08f
Hana-no-Git <flower127127@daum.net>
2020-10-14 19:43:18 +0900

    2020 10 14

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+
+
+// 하트 변환 
+const heart = document.querySelector('.heart_btn');
+const header = document.querySelector('#header');
+
+heart.addEventListener('click', function(){
+    console.log('hit');
+    heart.classList.toggle('on');
+});
+
+function scrollFunc(){
+    console.log(pageYOffset);
+
+    if(pageYOffset >= 10){
+        header.classList.add('on');
+    } else {
+        header.classList.remove('on');
+    }
+}
+
+window.addEventListener('scroll',scrollFunc);
